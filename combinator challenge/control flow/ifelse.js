65b861c42d6756dba8c2ba495b6438ff3afa04cb
Mariya Kashif <mariyakashif@Mariyas-MacBook-Air.local>
2023-08-29 10:30:03 +1000

    adds control-flow challenge

---
+++ combinator challenge/control flow/ifelse.js
@@ -0,0 +1,33 @@
+let n = 2;
+if (typeof n == "number") {
+  if (n % 2 == 0) {
+    console.log("n is even");
+  } else {
+    console.log("n is odd");
+  }
+} else {
+  console.log("invalid n");
+}
+
+//Check for smallest value
+let x = 2;
+let y = 3;
+
+if (x > y) {
+  console.log("x is greater than y");
+} else if (x == y) {
+  console.log("x is equal to y");
+} else {
+  console.log("x is smaller than y");
+}
+
+//User input type
+let userInput = 4;
+
+if (typeof userInput == "number") {
+  console.log(userInput ** 2);
+} else if (typeof userInput == "string") {
+  console.log(userInput);
+} else {
+  console.log("Invalid input");
+}
